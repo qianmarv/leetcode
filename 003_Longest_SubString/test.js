5e2f6776101b933960af8c9856fc15d5fbd469c1
qianmarv <qianmarv@gmail.com>
2018-06-12 08:50:39 +0800

    Improved Performance For Problem 3

--- 003_Longest_SubString/test.js
+++ 003_Longest_SubString/test.js
@@ -13,4 +13,21 @@ describe("Get Length of Longest SubString", function(){
             assert.equal(solution.getLengthOfLongestSubstring_1('pwwkew'), 3);
         });
     });
+    describe('Solution 2', function(){
+        it('`abcabcbb` should return 3', function(){
+            assert.equal(solution.glols_2('abcabcbb'), 3);
+        });
+        it('`bbbbb` should return 1', function(){
+            assert.equal(solution.glols_2('bbbbb'), 1);
+        });
+        it('`pwwkew` should return 3', function(){
+            assert.equal(solution.glols_2('pwwkew'), 3);
+        });
+        it('`a` should return 1', function(){
+            assert.equal(solution.glols_2('a'), 1);
+        });
+        it('`aab` should return 2', function(){
+            assert.equal(solution.glols_2('aab'), 2);
+        });
+    });
 });
