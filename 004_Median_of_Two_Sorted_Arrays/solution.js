69d92700b3c82a7d44f89b6fa403536adca93bac
Marvin Qian <qianmarv@gmail.com>
2018-06-19 05:56:03 +0800

    WIP Submit Problem 4 Median of Two Sorted Arrays

---
+++ 004_Median_of_Two_Sorted_Arrays/solution.js
@@ -0,0 +1,63 @@
+/*
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+ Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+ Example 1:
+ nums1 = [1, 3]
+ nums2 = [2]
+ The median is 2.0.
+
+ Example 2:
+ nums1 = [1, 2]
+ nums2 = [3, 4]
+ The median is (2 + 3)/2 = 2.5
+*/
+
+module.exports = {
+    // Relay on the embedded sort algorithms
+    // Lazy and cunning
+    findMedianSortedArrays1: function(nums1, nums2){
+
+        let nums = nums1.concat(nums2);
+        nums.sort(function(a,b){return a - b});
+        console.log(nums);
+        let len  = nums.length;
+        if(len % 2 === 0){
+            let n  = len / 2;
+            return (nums[n-1] + nums[n])/2;
+        }else{
+            return nums[Math.floor(len / 2)];
+        }
+    },
+    // dived into two parts, with same length
+    findMedianSortedArrays2: function(nums1, nums2){
+        //swap
+        if(nums1.length > nums2.length){
+            let tmp = nums2;
+            nums2 = nums1;
+            nums1 = tmp;
+        }
+        console.log(nums1);
+        console.log(nums2);
+        let m = nums1.length;
+        let n = nums2.length;
+        let iLeft  = 0;
+        let iRight =  m-1;
+        while(true){
+            let i = Math.floor((iLeft + iRight)/2); // initial
+            let j = Math.floor((m+n-i*2)/2);
+            console.log("i="+i+";j="+j);
+            if( i === m || i === 0 ||
+                (nums1[i] <= nums2[j+1] &&
+                 nums2[j] <= nums1[i+1])){//TODO Boundary i = m!!
+                //find it
+                return [i,j];
+            }else if(nums1[i] > nums2[j+1]){//  Move to left
+                iRight = i;
+            }else if(nums2[j] > nums1[i+1]){ // Move to Right
+                iLeft = i;
+            }
+        }
+    }
+}
---
+++ 004_Median_of_Two_Sorted_Arrays/test.js
@@ -0,0 +1,17 @@
+let assert = require('assert');
+let solution = require('./solution.js');
+
+describe("Get Median of Two Sorted Arrays", function(){
+    describe('Solution 1', function(){
+        it('[1, 3] & [2] should return 2', function(){
+            assert.equal(solution.findMedianSortedArrays1([1,3], [2]), 2);
+        });
+        it('[1, 2] & [3, 4] should return 2.5', function(){
+            assert.equal(solution.findMedianSortedArrays1([1,2], [3,4]), 2.5);
+        });
+
+        it('[1] & [2,3,4,5,6,7,8,9,10] should return 5.5', function(){
+            assert.equal(solution.findMedianSortedArrays1([1], [2,3,4,5,6,7,8,9,10]), 5.5);
+        });
+    });
+});
