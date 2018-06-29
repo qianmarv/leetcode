ad98de457c8bc9af60581b4643fb79f1854e529e
qianmarv <qianmarv@gmail.com>
2018-06-29 19:33:31 +0800

    WIP

--- 005_Longest_Palindromic_Substring/solution.js
+++ 005_Longest_Palindromic_Substring/solution.js
@@ -26,32 +26,60 @@ module.exports = {
             return true;
         },
         longestPalindrome_1: function (s) {
-            let result = [0,0];
-            let cachePairs
-            for (let i = 0; i < s.length; i++) {
+            // Outside - In
+            let result = [0, 0];
+            for (let i = 0; i < s.length && result[1] - result[0] + 1 < s.length - i; i++) {
                 let j = s.lastIndexOf(s[i]);
-                if(result[1] - result[0] + 1 > s.length - i){
-                    break;
-                }
-                for (; j > i; j = s.lastIndexOf(s[i], j-1)) {
-                    if(result[1] - result[0] > j - i){
-                        break;
-                    }
+                for (; j > i && result[1] - result[0] < j - i; j = s.lastIndexOf(s[i], j - 1)) {
                     var isPalindromic = true;
-                    for (let m = i+1, n = j-1; m <= n; m++, n--) { // Time consumer
+                    for (let m = i + 1, n = j - 1; m <= n; m++, n--) { // Time consumer
                         if (s[m] !== s[n]) {
-                             isPalindromic = false;
-                             break;
+                            isPalindromic = false;
+                            break;
                         }
                     }
-//                    console.log("substring="+s.substring(i,j+1)+";Palindromic: "+isPalindromic);
-                    // is Palindromic
-                    if(isPalindromic){
+                    if (isPalindromic) {
                         result = [i, j];
-                        break; // Outside > Inside
+                        break;
                     }
                 }
             }
-            return s.substring(result[0], result[1]+1);
+            return s.substring(result[0], result[1] + 1);
+        },
+        longestPalindrome_2: function (s) {
+            //Inside - Out
+            let result = [0, 1];
+            let farIndex = 0;
+            let nearIndex = 0;
+            let i = 1;
+            for (; i < s.length; i++) {
+                // 回文的条件：第一个/和前一个相等/和再往前一个相等/前一个在回文区域+回文区域前一个相等
+                if (s[i] === s[farIndex - 1]) { //Keep Rolling
+                    //                    console.log("substring: " + s.substring(farIndex, i+1));
+                    farIndex--;
+                } else if (s[i] === s[nearIndex - 1]) {
+                    farIndex = nearIndex - 1;
+                } else {
+                    if (i - farIndex > result[1] - result[0]) {
+                        result[0] = farIndex;
+                        result[1] = i;
+                    }
+                    farindex = i;
+                }
+
+                if (s[i] === s[i - 2]) {
+                    nearIndex = i - 2;
+                } else if (s[i] === s[i - 1]) {
+                    nearIndex = i - 1;
+                } else {
+                    nearIndex = -1;
+                }
+            }
+            if (i - farIndex > result[1] - result[0]) {
+                result[0] = farIndex;
+                result[1] = i;
+            }
+
+            return s.substring(result[0], result[1]);
         }
 }
--- 005_Longest_Palindromic_Substring/test.js
+++ 005_Longest_Palindromic_Substring/test.js
@@ -1,22 +1,40 @@
 let assert = require('assert');
-let solution = require('./solution.js');
+let solutions = require('./solution.js');
 
 describe("Longest Palindromic Substring", function () {
+            let cases = [
+                ["", ""],
+                ["a", "a"],
+                ["aa", "aa"],
+                ["aaa", "aaa"],
+                ["aaaa", "aaaa"],
+                ["aaaaa", "aaaaa"],
+                ["aaaaaa", "aaaaaa"],
+                ["abaa", "aba"],
+                ["aaabaaaa", "aaabaaa"],
+                ["baaabaaa", "aaabaaa"],
+                ["aaxaaxaa", "aaxaaxaa"],
+                ["abbaxabbaxabbaxabba", "abbaxabbaxabbaxabba"],
+                ["abaxabaxaba", "abaxabaxaba"],
+                ["abcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyzabcdefghijklmnokprstuvwxyz", "a"],
+                ["eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeebeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+                 "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeebeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+                ]
+            ];
+
             describe("Test Solution 1", function () {
-                let solution1 = solution.longestPalindrome_1;
-                let cases = [
-                    ["aa", "aa"],
-                    ["", ""],
-                    ["a", "a"],
-                    ["abaa", "aba"],
-                    ["aaabaaaa", "aaabaaa"],
-                    ["baaabaaa", "aaabaaa"],
-                    ["eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeebeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-                     "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeebeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"]
-                ];
+                let solution = solutions.longestPalindrome_1;
                 for (let i = 0; i < cases.length; i++) {
                     it(cases[i][0] + " should return " + cases[i][1], function () {
-                        assert.equal(solution1(cases[i][0]), cases[i][1]);
+                        assert.equal(solution(cases[i][0]), cases[i][1]);
+                    });
+                }
+            });
+            describe("Test Solution 2", function () {
+                let solution = solutions.longestPalindrome_2;
+                for (let i = 0; i < cases.length; i++) {
+                    it(cases[i][0] + " should return " + cases[i][1], function () {
+                        assert.equal(solution(cases[i][0]), cases[i][1]);
                     });
                 }
             })
