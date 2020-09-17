66744f83cb3846ce24b7a351c8e06438322759d1
Marvin Qian <qianmarv@gmail.com>
2020-09-17 14:53:41 +0800

    Update Problem 064

--- 064_Minimum_Path_Sum/Java/Solution.java
+++ 064_Minimum_Path_Sum/Java/Solution.java
@@ -1,5 +1,6 @@
 class Solution {
     private int [][] dp;
+    //Runtime: 1 ms, faster than 99.12% of Java online submissions for Minimum Path Sum.
     public int minPathSum(int[][] grid) {
         int nRows = grid.length;
         int nCols = grid[0].length;
@@ -32,5 +33,28 @@ class Solution {
         }
         dp[row][col] = min + grid[row][col];
         return dp[row][col];
+    }
+
+    //Runtime: 2 ms, faster than 87.42% of Java online submissions for Minimum Path Sum.
+    public mpsDP(int[][] grid){
+        int result = 0;
+        int nRows = grid.length;
+        int nCols = grid[0].length;
+        for(int i = 0; i < nRows; i++){
+            for(int j = 0; j < nCols; j++){
+                int minTop = 0;
+                int minLeft = 0;
+                if(i == 0 && j == 0){
+                    continue;
+                }else if(i == 0){
+                    grid[i][j] += grid[i][j-1];
+                }else if(j == 0){
+                    grid[i][j] += grid[i-1][j];
+                }else{
+                    grid[i][j] += Math.min(grid[i][j-1], grid[i-1][j]) ;
+                }
+            }
+        }
+        return grid[nRows-1][nCols-1];
     }
 }
