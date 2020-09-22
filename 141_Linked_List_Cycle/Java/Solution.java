77a5769296cb3179e87d224567a4e76a1ef42c19
Marvin Qian <qianmarv@gmail.com>
2020-09-22 08:40:23 +0800

    Solve Problem 141

---
+++ 141_Linked_List_Cycle/Java/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    // Runtime: 0 ms, faster than 100.00%
+    // Memory Usage: 39.2 MB, less than 95.63%
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) return true;
+        }
+        return false;
+    }
+}
