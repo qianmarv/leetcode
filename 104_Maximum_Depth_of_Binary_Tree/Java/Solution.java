a6e04824547c6d008e51a6712fa11a438a2e61c1
Marvin Qian <qianmarv@gmail.com>
2020-10-27 20:15:13 +0800

    Solve Problem 104

---
+++ 104_Maximum_Depth_of_Binary_Tree/Java/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //Runtime: 0 ms, faster than 100.00%
+    //Memory Usage: 39 MB, less than 10.32%
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        else return Math.max(maxDepth(root.left)+1, maxDepth(root.right)+1);
+    }
+}
