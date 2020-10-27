747c60b13e1658739b0049b23fd39c2791a8e471
Marvin Qian <qianmarv@gmail.com>
2020-10-28 07:58:10 +0800

    Solve Problem 226

---
+++ 226_Invert_Binary_Tree/Java/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //Runtime: 0 ms, faster than 100.00%
+    //Memory Usage: 36.2 MB, less than 10.95%
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode l = invertTree(root.left);
+        TreeNode r = invertTree(root.right);
+        root.left = r;
+        root.right = l;
+        return root;
+    }
+}
