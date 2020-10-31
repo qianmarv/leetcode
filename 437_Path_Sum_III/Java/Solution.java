f4c5164978622ac74a1c5cefcfbf79671b6d4c76
Marvin Qian <qianmarv@gmail.com>
2020-10-31 15:42:11 +0800

    Solve Problem 437

---
+++ 437_Path_Sum_III/Java/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //Runtime: 20 ms, faster than 39.09%
+    //Memory Usage: 38.9 MB, less than 9.75%
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) return 0;
+        int ret = pathSumStartWithRoot(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+        return ret;
+    }
+
+    private int pathSumStartWithRoot(TreeNode root, int sum) {
+        if (root == null) return 0;
+        int ret = 0;
+        if (root.val == sum) ret++;
+        ret += pathSumStartWithRoot(root.left, sum - root.val) + pathSumStartWithRoot(root.right, sum - root.val);
+        return ret;
+    }
+}
